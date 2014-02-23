f2f455d560d2f87ff641cba7cd073d00af4ed3fa
jam643d <jam643d@gmail.com>
2014-02-23 13:01:14 -0500

    Initial commit

---
+++ src/HearthstoneOCR.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: James
+ * Date: 2/20/14
+ * Time: 8:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HearthstoneOCR {
+}
---
+++ src/ImageConverter.java
@@ -0,0 +1,5 @@
+/**
+ * Created by James on 2/21/14.
+ */
+public class ImageConverter {
+}
